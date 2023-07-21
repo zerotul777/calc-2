d95d4b12fd6396725f48663f4cfb7fd762c2aa83
Max <139768623+zerotul777@users.noreply.github.com>
2023-07-21 16:31:44 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,90 @@
+let a = ''; // first number
+let b = ''; // second number
+let sign = ''; // знак операции 
+let finish = false; 
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+// экран 
+const out = document.querySelector('.calc-screen p');
+
+function clearAll () {
+    a = ''; // first number and result
+    b = ''; // second number
+    sign = ''; // знак 
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    // нажата не кнопка
+    if(!event.target.classList.contains('btn')) return;
+    // нажата кнопка clrarAll ac
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+
+    // получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    // если нажата клавиша 0-9 или .
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+            console.log(a, b, sign);
+            out.textContent = a;
+        }
+        else if (a!== '' && b!=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b, sign);
+        return;
+    }
+
+    // если нажата клавиша + - / X
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b, sign);
+        return;
+    }
+
+    // нажата =
+    if (key === '=') {
+        if (b === '') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "X":
+                a = a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Дибил';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b, sign);
+    }
+
+}
